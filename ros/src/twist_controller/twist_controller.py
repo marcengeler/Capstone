567725fdb6a71294d39c4719cf87c39af603959a
Marc Engeler <marc.engeler@gmail.com>
2018-02-16 20:12:24 +0100

    parameter tuning

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -26,26 +26,24 @@ class Controller(object):
         self.max_vel = kwargs['max_vel']
         self.accel_limit = kwargs['accel_limit']
         self.decel_limit = kwargs['decel_limit']
-        self.TP1 = LowPassFilter(0.5, 0.2)
+        self.TP1 = LowPassFilter(0.2, 0.05)
 
     def control(self, *args, **kwargs):
-        if self.time is None:
+        linear_velocity = kwargs['linear_velocity']
+        angular_velocity = kwargs['linear_velocity']
+        current_velocity = kwargs['current_velocity']
+        dbw_state = kwargs['dbw_state']
+
+        if self.time is None or not dbw_state:
             self.time = rospy.get_time()
             return 0.0, 0.0, 0.0
             
         dt = rospy.get_time() - self.time
 
-        linear_velocity = kwargs['linear_velocity']
-        angular_velocity = kwargs['linear_velocity']
-        current_velocity = kwargs['current_velocity']
-        dbw_state = kwargs['dbw_state']
-        
         velocity_margin = min(linear_velocity.x, self.max_vel) - current_velocity.x
-
-        rospy.loginfo("Velocity Margin: %f", velocity_margin)
         # Take out limits to develop
-        # velocity_margin = min(velocity_margin, self.accel_limit * dt)
-        # velocity_margin = max(velocity_margin, self.decel_limit * dt)
+        velocity_margin = min(velocity_margin, self.accel_limit * dt)
+        velocity_margin = max(velocity_margin, self.decel_limit * dt)
         
         throttle = self.throttle_control.step(velocity_margin, dt)
         steer = self.steering_control.get_steering(linear_velocity.x, angular_velocity.z, current_velocity.x)
@@ -60,5 +58,4 @@ class Controller(object):
             brake = 0.0
         
         self.time = rospy.get_time()
-        rospy.loginfo("Throttle: %f, Brake: %f, Steer: %f", throttle, brake, steer)
         return throttle, brake, steer
