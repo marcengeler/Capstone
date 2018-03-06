0ac17bfcdd87354cb2ed7bb39f3bd8d2c62efb13
artur <babalasoft@gmail.com>
2018-03-06 19:26:21 +0100

    Update model_classification, init car_position

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -161,6 +161,7 @@ class TLDetector(object):
         """
         light = None
         closest_light = -1
+        car_position = -1
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_light_positions = self.config['stop_line_positions']
 
