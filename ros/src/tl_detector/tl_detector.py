8a33670deabc798809eb26cec1787920553b740a
artur <babalasoft@gmail.com>
2018-03-16 15:47:49 -0700

    Add code to use ground trouth of tl, provided by sumulator.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -11,11 +11,13 @@ import tf
 import cv2
 import yaml
 import math
+import time
 
 STATE_COUNT_THRESHOLD = 3
 
 #Traffic Light detection range
 TL_DETECTION_RANGE = 50
+USE_CLASSIFIER = True
 
 class TLDetector(object):
     def __init__(self):
@@ -37,7 +39,7 @@ class TLDetector(object):
         rely on the position of the light and the camera image to predict it.
         '''
         sub3 = rospy.Subscriber('/vehicle/traffic_lights', TrafficLightArray, self.traffic_cb)
-        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
+        sub6 = rospy.Subscriber('/image_color', Image, self.image_cb, queue_size=1)
 
         config_string = rospy.get_param("/traffic_light_config")
         self.config = yaml.load(config_string)
@@ -122,7 +124,7 @@ class TLDetector(object):
             waypoint_x = waypoint.pose.pose.position.x
             waypoint_y = waypoint.pose.pose.position.y
 
-            dist_to_waypoint = math.sqrt((waypoint_x - x) ** 2 + (waypoint_y - y) ** 2)
+            dist_to_waypoint = self.euclidianDistance(waypoint_x, waypoint_y, x, y)
 
             # Initialize minimum distance at first value, or get new minimum distance
             if minimum_distance is None:
@@ -154,15 +156,13 @@ class TLDetector(object):
         return self.light_classifier.get_classification(cv_image)
 
     def get_distance(self, wp1_idx, wp2_idx):
-        """Determines the distance between two points
-
+        """Determines the distance between two way point's index
         Args:
             wp1_idx (waypoint index): waypoint index 1
             wp2_idx (waypoint index): waypoint index 2
 
         Returns:
             int: distance in meters between two waypoint index
-
         """
         if(wp1_idx < 0 or wp1_idx > len(self.waypoints) or
            wp2_idx < 0 or wp2_idx > len(self.waypoints)):
@@ -170,28 +170,23 @@ class TLDetector(object):
 
         wp1 = self.waypoints[wp1_idx].pose.pose.position
         wp2 = self.waypoints[wp2_idx].pose.pose.position
-        dist = math.sqrt((wp1.x - wp2.x) ** 2 + (wp1.y - wp2.y) ** 2)
-        return dist
+        return self.euclidianDistance(wp1.x, wp1.y, wp2.x, wp2.y)
 
-    def process_traffic_lights(self):
-        """Finds closest visible traffic light, if one exists, and determines its
-            location and color
+    def euclidianDistance(self, x1, y1, x2, y2):
+        return math.sqrt((x1 -x2)**2 + (y1 - y2)**2)
 
+    def find_closest_traffic_light(self, car_position):
+        """Determines closest traffic to the car position
+        Args:
+            car_position: actual car position
         Returns:
-            int: index of waypoint closest to the upcoming stop line for a traffic light (-1 if none exists)
-            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
-
+            ligt: position of the traffic light
+            light_waypoint: closest waypoint to the traffic light
         """
         light = None
         closest_light = -1
-        car_position = -1
-        distance_to_tl = -1
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_light_positions = self.config['stop_line_positions']
-
-        if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
-
         # Loop through all the stop light positions
         for i,stop_light_position in enumerate(stop_light_positions):
             # Initialize a Pose
@@ -210,16 +205,65 @@ class TLDetector(object):
                 elif light_waypoint < closest_light:
                     closest_light = light_waypoint
                     light = light_pose
+        return light, closest_light
 
-        if car_position and light:
+    def get_traffic_light_ground_truth(self, traffic_light):
+        """
+        Finds ground truth from the traffic light
+        Args:
+            traffic_light: traffic light position
+        Returns:
+            light.state: traffic light state
+        """
+        tl_x = traffic_light.position.x
+        tl_y = traffic_light.position.y
+
+        state = TrafficLight.UNKNOWN
+        for light in self.lights:
+            '''
+            If position of the light from the yaml file and one roperted via
+            /vehicle/traffic_lights differs only within 30 m consider them as same
+            '''
+            l_x = light.pose.pose.position.x
+            l_y = light.pose.pose.position.y
+            if self.euclidianDistance(l_x, l_y, tl_x, tl_y) < 30:
+                return light.state
+        return state
+
+    def process_traffic_lights(self):
+        """
+        Finds closest visible traffic light, if one exists, and determines its
+            location and color
+
+        Returns:
+            int: index of waypoint closest to the upcoming stop line for a traffic light (-1 if none exists)
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+        """
+        if (self.has_image == False):
+            return -1, TrafficLight.UNKNOWN
+
+        light_pos = None
+        closest_light = -1
+        car_position = -1
+        distance_to_tl = -1
+
+        if(self.pose):
+            car_position = self.get_closest_waypoint(self.pose.pose)
+
+        light_pos, closest_light = self.find_closest_traffic_light(car_position)
+
+        if car_position and light_pos:
             waypoint_num_to_light = abs(car_position - closest_light)
             distance_to_tl = self.get_distance(car_position, closest_light)
 
-        if light:
+        if light_pos:
             state = TrafficLight.UNKNOWN
-            if (distance_to_tl < TL_DETECTION_RANGE):
-                state = self.get_light_state(light)
-                rospy.loginfo("-dist to closest TL- " + str(distance_to_tl))
+            if USE_CLASSIFIER:
+                if (distance_to_tl < TL_DETECTION_RANGE):
+                    state = self.get_light_state(light_pos)
+                    #rospy.loginfo("-dist to closest TL- " + str(distance_to_tl))
+            else:
+                state = self.get_traffic_light_ground_truth(light_pos)
             return closest_light, state
         return -1, TrafficLight.UNKNOWN
 
